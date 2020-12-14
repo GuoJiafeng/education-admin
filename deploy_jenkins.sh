519bda82decd31c1bf7aa6685e0aca1a629b2542
gjf <gjf>
2020-12-14 22:12:23 +0800

    update

--- deploy_jenkins.sh
+++ deploy_jenkins.sh
@@ -2,17 +2,12 @@ export PATH=/root/node-v12.8.1-linux-x64/bin:$PATH
 
 yarn config set puppeteer_download_host https://npm.taobao.org/mirrors
 
-yarn add puppeteer --ignore-scripts
 
-cd node_modules/puppeteer
-
-yarn exec node install.js
+yarn 
 
 # 安装项目中的依赖
 ##yarn
-cd ..
 
-cd ..
 # 打包
 yarn build
 
